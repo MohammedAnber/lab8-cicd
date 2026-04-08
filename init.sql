3be3c59a840e72d77f8a6ed9c0373f0de2da1961
MohammedAnber <144551633+MohammedAnber@users.noreply.github.com>
2026-04-08 10:01:47 +0200

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,5 +1,14 @@
-INSERT INTO tasks (name, status) VALUES
-  ('Milk', 'pending'),
-  ('Tea', 'done'),
-  ('Oil', 'pending'),
-  ('Coffee', 'pending');
+-- 1. مسح الجدول لو موجود عشان نبدأ على نظافة
+DROP TABLE IF EXISTS tasks;
+
+-- 2. إنشاء الجدول
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    status VARCHAR(50) DEFAULT 'pending'
+);
+
+
+INSERT INTO tasks (name, status) VALUES ('Milk', 'pending');
+INSERT INTO tasks (name, status) VALUES ('Tea', 'done');
+INSERT INTO tasks (name, status) VALUES ('Oil', 'pending');I
